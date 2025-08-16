7618850396881cadce92772f714da012266bcc85
Serhet <serhetgokdemir@hotmail.com>
2025-08-16 22:33:31 +0300

    Done First Module

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/serhet/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,21 @@
+package com.serhet.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose '/' that return "Hello World"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+
+    // expose a new endpoit for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){ return "...Deneme..."; }
+
+    @GetMapping("/serhet")
+    public String saySerhet(){return "Serhet Gökdemir - YTÜ";}
+}
