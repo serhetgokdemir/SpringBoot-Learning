2e978f390e6bd9b95190b643a3ded433281dfd89
Serhet <serhetgokdemir@hotmail.com>
2025-08-20 11:44:04 +0300

    Done DAY5

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,7 +42,14 @@ public class StudentRestController {
 
         // just index into the list. keep it simple for now.
 
+        // check the studentId again list size
+        if(studentId >= theStudents.size() || (studentId < 0)){
+            throw new StudentNotFoundException("Invalid student id - " + studentId);
+        }
+
         return theStudents.get(studentId);
     }
 
 }
+
+
